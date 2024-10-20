d7e77edac64768569a2fec5cc22d7683991fd3cb
Dandan-14 <1030dandan@gmail.com>
2024-10-19 20:37:12 -0500

    docs: commit 2 se subio el archivo

---
+++ suma en python.py
@@ -0,0 +1,12 @@
+//suma_de_dos_numeros
+
+
+# Pide al usuario que ingrese dos números
+num1 = float(input("Ingresa el primer número: "))
+num2 = float(input("Ingresa el segundo número: "))
+
+# Calcula la suma
+suma = num1 + num2
+
+# Muestra el resultado
+print(f"La suma de {num1} y {num2} es: {suma}")
